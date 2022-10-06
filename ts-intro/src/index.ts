9dcb1f5926c3264b5f9f3a7c70fd94a249be5394
OscarCasahe <oscar.casahe@gmail.com>
2022-10-06 09:13:52 +0200

    nuevos ejercicios

--- ts-intro/src/index.ts
+++ ts-intro/src/index.ts
@@ -1,41 +1,7 @@
+// import {Producto} from './ejercicios/06-';
+
 /*
     ===== Código de TypeScript =====
 */
 
-interface Producto {
-    
-    desc: string,
-    precio: number
-}
-
-const telefono: Producto = {
-
-    desc: 'Nokia A1',
-    precio: 200
-
-}
-
-const tablet: Producto = {
-
-    desc: 'IPad Air',
-    precio: 500
-
-}
-
-function calculaIVA(productos: Producto[]) {
-
-    let total = 0;
-
-    productos.forEach((producto) => {
-
-        total += producto.precio;
-    });
-
-    return total * 0.15;
-
-}
-
-const articulos = [telefono, tablet]
-const iva = calculaIVA(articulos);
-
-console.log('IVA: ', iva);
+const carritoCompras: Producto[] = [];
